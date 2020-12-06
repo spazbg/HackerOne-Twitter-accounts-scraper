8787ce602f14d839355ad09aad8e39354c1437ec
Spas Genov <spaz@mail.bg>
2020-12-06 19:17:41 +0200

    Add new H1 sources for scraping
    Now the script extracts Twitter accounts from these leaderboards:
    Highest Reputation
    Up and Comers
    Highest Critical Reputation
    Most Upvoted,
    Highest Critical Reputation

--- Program.cs
+++ Program.cs
@@ -23,66 +23,79 @@ namespace hackerone
             var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
                 chromeOptions);
 
-
-            driver.Navigate().GoToUrl("https://hackerone.com/leaderboard/reputation"); //Highest Reputation
-            //driver.Navigate().GoToUrl("https://hackerone.com/leaderboard/up_and_comers"); //Up and Comers
-            //driver.Navigate().GoToUrl("https://hackerone.com/leaderboard/high_critical"); //Highest Critical Reputation
-            //driver.Navigate().GoToUrl("https://hackerone.com/leaderboard/upvotes"); //Most Upvoted
-            
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
-
-            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.XPath("//tbody['daisy-table-body']")));
-
             var allTwitterLinks = new List<string>();
 
-            var hackers = driver.FindElements(By.XPath("//tbody['daisy-table-body']/tr//a"));
-            
-
-            for (int i = 0; i < hackers.Count; i++)
+            var urlsToScrape = new List<string>()
             {
-                try
+                "https://hackerone.com/leaderboard/reputation", //Highest Reputation
+                "https://hackerone.com/leaderboard/up_and_comers",  //Up and Comers
+                "https://hackerone.com/leaderboard/high_critical", //Highest Critical Reputation
+                "https://hackerone.com/leaderboard/upvotes", //Most Upvoted,
+                "https://hackerone.com/leaderboard/high_critical" //Highest Critical Reputation
+            };
+
+
+            foreach (var url in urlsToScrape)
+            {
+                driver.Navigate().GoToUrl(url);
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+                //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+
+                WebDriverWait wait = new WebDriverWait(driver,
+                    TimeSpan.FromSeconds(3));
+                wait.Until((x) =>
                 {
-                    hackers = driver.FindElements(By.XPath("//tbody['daisy-table-body']/tr//a"));
-                    hackers[i].Click();
+                    return ((IJavaScriptExecutor)driver).ExecuteScript(
+                        "return document.readyState").Equals("complete");
+                });
 
-                    wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(
-                        By.XPath("//div/div[@class='card__content']")));
-                    var hackerCard = driver.FindElements(By.XPath("//div/div[@class='card__content']")).First();
 
-                    wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
-                    wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(
-                        By.XPath("//a[@class='daisy-link ahref margin-5--left margin-5--right spec-twitter-handle']")));
+                wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.XPath("//tbody['daisy-table-body']")));
+                
+                var hackers = driver.FindElements(By.XPath("//tbody['daisy-table-body']/tr//a"));
+                
 
-                    var twitterIcon =
-                        hackerCard.FindElement(
-                            By.XPath(
-                                "//a[@class='daisy-link ahref margin-5--left margin-5--right spec-twitter-handle']"));
-                    var twitterLink = twitterIcon.GetAttribute("href");
-                    allTwitterLinks.Add(twitterLink);
-                    driver.Navigate().Back();
-                }
-                catch
+                for (int i = 0; i < hackers.Count; i++)
                 {
-                    driver.Navigate().Back();
-                    continue;
+                    try
+                    {
+                        hackers = driver.FindElements(By.XPath("//tbody['daisy-table-body']/tr//a"));
+                        hackers[i].Click();
+
+                        wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(
+                            By.XPath("//div/div[@class='card__content']")));
+                        var hackerCard = driver.FindElements(By.XPath("//div/div[@class='card__content']")).First();
+
+                        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+                        wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(
+                            By.XPath("//a[@class='daisy-link ahref margin-5--left margin-5--right spec-twitter-handle']")));
+
+                        var twitterIcon =
+                            hackerCard.FindElement(
+                                By.XPath(
+                                    "//a[@class='daisy-link ahref margin-5--left margin-5--right spec-twitter-handle']"));
+                        var twitterLink = twitterIcon.GetAttribute("href");
+                        allTwitterLinks.Add(twitterLink);
+                        driver.Navigate().Back();
+                    }
+                    catch
+                    {
+                        driver.Navigate().Back();
+                        continue;
+                    }
+
                 }
 
             }
-
 
             var location = new Uri(Assembly.GetEntryAssembly().GetName().CodeBase);
             var directoryFullName = new FileInfo(location.AbsolutePath).Directory.FullName;
 
             var pathFile = $@"{directoryFullName}\HackerOne_{DateTime.Now.ToString("dd-MM-yyyy-HH-mm")}.txt";
-        
+
             File.AppendAllLines(pathFile, allTwitterLinks.Distinct(), Encoding.UTF8);
-
-            //var uniqueTwitters = new HashSet<string>();
-            //File.WriteAllLines(directoryFullName, File.ReadLines(pathFile)
-            //    .Where(line => uniqueTwitters.Add(line)));
-
+            
             driver.Quit();
 
         }
